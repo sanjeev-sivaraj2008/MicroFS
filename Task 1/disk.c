16be06771858b761f3723ab12c76c72da7b28bfe
sanjeev-sivaraj2008 <sanjeev.sivaraj.2008@gmail.com>
2026-03-30 14:38:15 +0530

    added disk_read_slot and disk_write_slot functions to read and write 1 slot from disk.bin

--- Task 1/disk.c
+++ Task 1/disk.c
@@ -3,13 +3,13 @@
 #include <stdbool.h>
 #include<stdint.h>
 
-typedef struct __attribute__((packed))
+typedef struct 
 {
     char name[24];
-    bool in_use;
+    uint8_t in_use;
     uint8_t padding[3];
     char content[484];
-} Slot  ;
+} __attribute__((packed)) Slot  ;
 static_assert(sizeof(Slot)==512,"Size is not 512 bytes");
 
 void disk_init()
@@ -18,7 +18,7 @@ void disk_init()
     if (fp!=NULL)
     {
         fclose(fp);
-    
+        return;
     }
     else
     {
@@ -26,27 +26,35 @@ void disk_init()
         Slot data[8] = {0};
         fwrite(data,sizeof(Slot),8,fp);
         fclose(fp);
+        return;
     }
 }
-// int disk_read_slot(struct Slot *p,int index)
-// {
-//     int slot;
-//     FILE *fp=fopen("disk.bin","wb");
-//     fseek(fp,index*512,SEEK_SET);
-//     fread(&slot,sizeof(Slot),1,fp);
-//     fclose(fp);
-//     return slot;
-// }
-// int disk_write_slot(int index,int slot)
-// {
-//     FILE *f = fopen("disk.bin", "r+b");
-//     fseek(f, index * 512, SEEK_SET);
-//     fwrite(&slot, sizeof(Slot), 1, f);
-//     fclose(f);
-// }
+void disk_read_slot(Slot *p,int index)
+{
+    
+    FILE *fp=fopen("disk.bin","rb");
+    fseek(fp,index*512,SEEK_SET);
+    fread(p,sizeof(Slot),1,fp);
+    fclose(fp);
+    
+}
+int disk_write_slot(Slot *p,int index)
+{
+    FILE *f = fopen("disk.bin", "r+b");
+    fseek(f, index * 512, SEEK_SET);
+    fwrite(p, sizeof(Slot), 1, f);
+    fclose(f);
+}
 int main()
 {
     disk_init();
+    Slot test1,test2;
+    strncpy(test1.name,"struct",6);
+    test1.in_use=1;
+
+    disk_write_slot(&test1,2);
+    disk_read_slot(&test2,2);
+    printf("%s",test2.name);
     
     return 0;
 }
